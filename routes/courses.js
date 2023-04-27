8e4dcd9c1fc2cbf28b75b3584ae332c6c7a3051e
Doug Beckwith <dougcbeckwith@gmail.com>
2023-04-27 12:28:35 -0700

    get courses routes working

--- routes/courses.js
+++ routes/courses.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const Courses = require("../models").Course;
+const Course = require("../models").Course;
 const User = require("../models").User;
+
+const { authenticateUser } = require("../middleware/user-auth");
 
 // get all courses including the user associated with each course
 router.get("/", async (req, res, next) => {
   try {
-    const courses = await Courses.findAll({
+    const courses = await Course.findAll({
       include: [
         {
           model: User,
@@ -15,64 +17,127 @@ router.get("/", async (req, res, next) => {
         }
       ]
     });
-    console.log(JSON.stringify(courses, null, 2));
-    res.status(200).json({ message: "Success" });
-  } catch (err) {
-    console.log(err);
-    next(err);
+    res.status(200).json({ courses });
+  } catch (error) {
+    console.log("ERROR: ", error.name);
+
+    if (
+      error.name === "SequelizeValidationError" ||
+      error.name === "SequelizeUniqueConstraintError"
+    ) {
+      const errors = error.errors.map((err) => err.message);
+      res.status(400).json({ errors });
+    } else {
+      throw error;
+    }
   }
 });
 
-// get one course and user associated with it
+// get course by id
 router.get("/:id", async (req, res, next) => {
   try {
-    // const courses = await Course.findAll();
-    res.status(200).json({ message: "Success" });
-  } catch (err) {
-    console.log(err);
-    next(err);
+    const course = await Course.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: "user"
+        }
+      ]
+    });
+    if (!course) {
+      res.status(400).json({ message: "No Course Found" });
+    } else {
+      res.status(200).json({ course });
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
-// post to create course
-router.post("/", async (req, res, next) => {
+// create course
+router.post("/", authenticateUser, async (req, res, next) => {
   try {
-    // Find all users
-    // console.log(req.body);
-    // const { title, description, estimatedTime, materialsNeeded } = req.body;
-    // const course = await course.create({
-    //   title,
-    //   description,
-    //   estimatedTime,
-    //   materialsNeeded
-    // });
-    // console.log(user);
-    // res.location("/");
-    // res.status(201).end();
-    res.status(200).json({ message: "Success" });
-  } catch (err) {
-    console.log(err);
-    next(err);
+    const course = await Course.create(req.body);
+    res.status(201).location(`${course.id}`).end();
+  } catch (error) {
+    console.log("ERROR: ", error.name);
+
+    if (
+      error.name === "SequelizeValidationError" ||
+      error.name === "SequelizeUniqueConstraintError"
+    ) {
+      const errors = error.errors.map((err) => err.message);
+      res.status(400).json({ errors });
+    } else {
+      throw error;
+    }
   }
 });
 
 // update course
-router.put("/:id", async (req, res, next) => {
+router.put("/:id", authenticateUser, async (req, res, next) => {
   try {
-    res.status(200).json({ message: "Success" });
-  } catch (err) {
-    console.log(err);
-    next(err);
+    const course = await Course.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: "user"
+        }
+      ]
+    });
+
+    if (!course) {
+      res.status(400).json({ message: "No Course Found" });
+    }
+    if (course.userId === req.currentUser.id) {
+      course.set({
+        title: req.body.title,
+        description: req.body.description,
+        userId: req.body.userId
+      });
+      await course.save();
+      res.status(204).end();
+    } else {
+      res.status(400).json({ message: "Not Authorized To Update Course" });
+    }
+  } catch (error) {
+    console.log("ERROR: ", error.name);
+
+    if (
+      error.name === "SequelizeValidationError" ||
+      error.name === "SequelizeUniqueConstraintError"
+    ) {
+      const errors = error.errors.map((err) => err.message);
+      res.status(400).json({ errors });
+    } else {
+      throw error;
+    }
   }
 });
 
 // delete course
-router.delete("/:id", async (req, res, next) => {
+router.delete("/:id", authenticateUser, async (req, res, next) => {
   try {
-    res.status(200).json({ message: "Success" });
-  } catch (err) {
-    console.log(err);
-    next(err);
+    const course = await Course.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: "user"
+        }
+      ]
+    });
+    if (!course) {
+      res.status(400).json({ message: "No Course Found" });
+    }
+    // check allowed to destory
+    if (course.userId === req.currentUser.id) {
+      await course.destroy();
+      res.status(204).end();
+    } else {
+      res.status(400).json({ message: "Not Authorized To Delete Course" });
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
