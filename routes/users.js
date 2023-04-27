e658ce0072af15f9483ab7ca74e1e4dd9eb0489d
Doug Beckwith <dougcbeckwith@gmail.com>
2023-04-27 12:28:22 -0700

    get user routes working

--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,41 @@
 const express = require("express");
 const router = express.Router();
-
+const { authenticateUser } = require("../middleware/user-auth");
 const User = require("../models").User;
+const bcrypt = require("bcrypt");
 
-router.get("/", async (req, res, next) => {
+// Get user protected
+router.get("/", authenticateUser, async (req, res, next) => {
   try {
-    // Find all users
-    const users = await User.findAll();
-    res.status(200).json({ users: users });
-  } catch (err) {
-    console.log(err);
-    next(err);
+    res.status(200).json({ user: req.currentUser });
+  } catch (error) {
+    console.log(error);
   }
 });
 
+// Create user
 router.post("/", async (req, res, next) => {
+  const user = req.body;
+
+  if (user.password) {
+    user.password = bcrypt.hashSync(user.password, 10);
+  }
+
   try {
-    // Find all users
-    console.log(req.body);
-    const { firstName, lastName, emailAddress, password } = req.body;
-    const user = await User.create({
-      firstName,
-      lastName,
-      emailAddress,
-      password
-    });
-    console.log(user);
-    res.location("/");
-    res.status(201).end();
-  } catch (err) {
-    console.log(err);
-    next(err);
+    await User.create(user);
+    res.status(201).location("/").end();
+  } catch (error) {
+    console.log("ERROR: ", error.name);
+
+    if (
+      error.name === "SequelizeValidationError" ||
+      error.name === "SequelizeUniqueConstraintError"
+    ) {
+      const errors = error.errors.map((err) => err.message);
+      res.status(400).json({ errors });
+    } else {
+      throw error;
+    }
   }
 });
 
